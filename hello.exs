427ccc428483c852529fbf7b096bfe145be34562
Willian <willianmetelien@gmail.com>
2026-06-28 22:39:12 -0400

    example commit

--- hello.exs
+++ hello.exs
@@ -1,8 +1,17 @@
-defmodule Hello do
-  def word do
-    IO.puts("HEllo elixir")
-  end
+# defmodule Hello do
+#   def world do
+#     # IO.puts("Hello, #{name}!")
+#     :error
+#   end
+# end
+
+#   Hello.world()
+
+
+defmodule SumServer do
+    use ExActor.GenServer
+    defstart start
+    defcall sum(a, b) do
+    reply(a + b)
+    end
 end
-
-
-Hello.word()
